9c13c857a4202a61c2cf329f962bd1c7219333ca
danielGibaru <daniel.gibaru.34@gmail.com>
2017-07-24 11:15:29 +0200

    Clonage du projet beweb

---
+++ module/Admin/src/Admin/Controller/LandingpageController.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+use Admin\Form\ArticleForm;
+use Admin\Model\ArticleModel;
+
+class LandingpageController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        // Entity manager
+        $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        $articleList = $em->getRepository('Application\Entity\BeziersLandingpageBw')->findBy(array(), array('position'=>'asc'));
+
+        // Appel de la vue
+        return new ViewModel(array( 'articleList' => $articleList,));
+    }
+
+    public function deleteAction()
+    {
+        $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+        $id = 0;
+        $message = "Impossible de supprimer l'article (Id invalide ?)";
+
+        if (!empty($_GET['id'])) {
+            $id = intval($_GET['id']);
+        }
+
+        if ($id > 0) {
+            try {
+                $article = $em->getRepository('Application\Entity\BeziersLandingpageBw')->find($id);
+                $em->remove($article);
+                $em->flush();
+                $message = "Article supprimé avec succès";
+            }
+            catch(\Exception $ex) {
+                $message = "Une erreur est survenue : " .  $ex->getMessage();
+            }
+
+        }
+
+        return new ViewModel(array('message' => $message));
+    }
+
+    public function editAction()
+    {
+        $id = 0;
+        $message = "";
+        if (!empty($_GET['id'])) {
+            $id = intval($_GET['id']);
+        }
+        $articleForm = new ArticleForm();
+
+        if ($id > 0) {
+            $em = $this->getServiceLocator()->get('Doctrine\ORM\EntityManager');
+            $article = $em->getRepository('Application\Entity\BeziersLandingpageBw')->find($id);
+
+            // Appel formulaire article
+            $articleForm->get('titre')->setValue($article->getTitre());
+            $articleForm->get('contenu')->setValue($article->getContenu());
+            $articleForm->get('image')->setValue($article->getImage());
+
+            $request = $this->getRequest();
+
+            // Si une requête de type post a été émise
+            if ($request->isPost()) {
+                $articleModel = new ArticleModel();
+                $articleForm->setInputFilter($articleModel->getInputFilter());
+                $articleForm->setData($request->getPost());
+
+                // Si formulaire valide
+                if ($articleForm->isValid()) {
+                    $articleModel->exchangeArray($articleForm->getData());
+
+                    // On crée un nouvel objet de type newsletter correspondant à notre entité
+                    $article->setTitre($articleModel->titre); // Email
+                    $article->setContenu($articleModel->contenu); // objet
+                    $article->setImage($articleModel->image); // message
+
+                    // On essaye d'inserer dans la bdd
+                    try {
+                        $em->persist($article);
+                        $em->flush();
+                        $message = "Votre article a été modifié avec succès";
+                    }
+                    catch(\Exception $ex) {
+                        $message = "Un erreur est survenue. " . $ex->getMessage();
+                    }
+                }
+                else {
+                    if(!$articleForm->isValid()) {
+                        $message = "Formulaire invalide";
+                    }
+                }
+            }
+            // Fin formulaire article
+        }
+        return new ViewModel(array('message' => $message,
+                                   'articleForm' => $articleForm));
+    }
+}
---
+++ module/Admin/src/Admin/Model/ArticleModel.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Admin\Model;
+
+ // Add these import statements
+ use Zend\InputFilter\InputFilter;
+ use Zend\InputFilter\InputFilterAwareInterface;
+ use Zend\InputFilter\InputFilterInterface;
+
+class ArticleModel implements InputFilterAwareInterface
+ {
+    public $titre;
+    public $contenu;
+    public $image;
+
+    protected $inputFilter;
+
+    public function exchangeArray($data)
+     {
+         $this->titre = (isset($data['titre']))  ? $data['titre']  : null;
+         $this->contenu = (isset($data['contenu']))  ? $data['contenu']  : null;
+         $this->image = (isset($data['image']))  ? $data['image']  : null;
+     }
+
+     // Add content to these methods:
+     public function setInputFilter(InputFilterInterface $inputFilter)
+     {
+         throw new \Exception("Not used");
+     }
+
+    public function getInputFilter()
+    {
+        if (!$this->inputFilter) {
+            $inputFilter = new InputFilter();
+
+            // Validation champ email
+            $inputFilter->add(array(
+               'name'     => 'titre',
+               'required' => true,
+               'filters'  => array(
+                  array('name' => 'StripTags'),
+                  array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+               array(
+                  'name'    => 'StringLength',
+                  'options' => array(
+                     'encoding' => 'UTF-8',
+                     'min'      => 2,
+                     'max'      => 255,
+                     ),
+                  ),
+               ),
+            ));
+
+            $inputFilter->add(array(
+               'name'     => 'contenu',
+               'required' => true,
+               'filters'  => array(
+                  array('name' => 'StripTags'),
+                  array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+               array(
+                  'name'    => 'StringLength',
+                  'options' => array(
+                     'encoding' => 'UTF-8',
+                     'min'      => 50,
+                     'max'      => 65000,
+                     ),
+                  ),
+               ),
+            ));
+
+
+            $inputFilter->add(array(
+               'name'     => 'image',
+               'required' => false,
+               'filters'  => array(
+                  array('name' => 'StripTags'),
+                  array('name' => 'StringTrim'),
+            ),
+            'validators' => array(
+               array(
+                  'name'    => 'StringLength',
+                  'options' => array(
+                     'encoding' => 'UTF-8',
+                     'min'      => 0,
+                     'max'      => 255,
+                     ),
+                  ),
+               ),
+            ));
+
+            $this->inputFilter = $inputFilter;
+        }
+
+        return $this->inputFilter;
+    }
+ }
